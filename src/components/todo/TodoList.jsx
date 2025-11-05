cc2e403cbe8bbd23d205827b81df71d25a752d67
Dilshad <dillu.iitv@gmail.com>
2025-11-05 22:06:21 +0530

    todos

--- src/components/todo/TodoList.jsx
+++ src/components/todo/TodoList.jsx
@@ -1,21 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styles from "./Todos.module.css";
 
 const TodoList = ({ todos }) => {
+  const [completed, setCompleted] = useState(() => todos.map(() => false));
+
+  useEffect(() => {
+    // Preserve existing completed states and add false for new items
+    setCompleted((prev) => {
+      const newCompleted = [...prev];
+      while (newCompleted.length < todos.length) {
+        newCompleted.push(false);
+      }
+      return newCompleted.slice(0, todos.length);
+    });
+  }, [todos]);
+
+  const toggleCompleted = (index) => {
+    setCompleted((prev) => {
+      const copy = [...prev];
+      copy[index] = !copy[index];
+      return copy;
+    });
+  };
   return (
     <>
       <div className="container overflow-hidden text-center mt-5">
-        <div className="row gy-5">
-          {todos.map((todo, index) => (
-            <div className="col-4" key={index}>
-              <div
-                className="p-3 bg-dark border-blue-500 border-2 text-white"
-                key={index}
-              >
-                {todo}
+        <form>
+          <div className="row gy-5">
+            {todos.map((todo, index) => (
+              <div className="col-3" key={index}>
+                <div className="p-3 bg-dark border-blue-500 border-2 text-white">
+                  <input
+                    type="checkbox"
+                    checked={!!completed[index]}
+                    onChange={() => toggleCompleted(index)}
+                    aria-label={`Toggle todo ${index}`}
+                  />
+                  <span
+                    className={
+                      completed[index] ? `${styles.completed} ms-2` : "ms-2"
+                    }
+                  >
+                    {todo}
+                  </span>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        </form>
       </div>
       {/* <ul>
         {todos.map((todo, index) => (
